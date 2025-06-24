35506b76584dd23ded9d3e777debab8bb5b540cd
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-06-24 16:24:32 +0900

    feat : 디렉토리 생성

---
+++ Assets/02.Scripts/Util/Define.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagName
+{
+    public const string Enemy = "Enemy";
+    public const string Player = "Player";
+}
+public static class LayerName
+{
+    public const string Enemy = "Enemy";
+    public const string Player = "Player";
+}
---
+++ Assets/02.Scripts/Util/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Wrapper<T>
+{
+    public T[] datas;
+}
+
+public static class JsonHelper
+{
+    // 배열 → JSON
+    public static string ToJson<T>(T[] array, bool prettyPrint = false)
+    {
+        var wrapper = new Wrapper<T> { datas = array };
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    // JSON → 배열
+    public static T[] FromJson<T>(string json)
+    {
+        var wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.datas;
+    }
+}
