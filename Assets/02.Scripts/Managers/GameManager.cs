97f9ec44a7075c2ea139d0a4920a01626d3b1c00
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-07-01 20:36:51 +0900

    Delete : 플레이어매니저 제거

--- Assets/02.Scripts/Managers/GameManager.cs
+++ Assets/02.Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [DefaultExecutionOrder(-100)]
 public class GameManager : MonoSingleton<GameManager>
@@ -10,9 +11,34 @@ public class GameManager : MonoSingleton<GameManager>
 
     public event Action onDestinyChange;
 
+    private GameObject _player;
+    public GameObject Player
+    {
+        get { return _player; }
+    }
     public void setCurDestinyData(DestinyData destinyData)
     {
         this.curDestinyData = destinyData;
         onDestinyChange?.Invoke();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(_player != null)
+        {
+            Destroy(_player);
+            _player=null;
+        }
+        _player = GameObject.FindGameObjectWithTag(TagName.Player);
     }
 }
