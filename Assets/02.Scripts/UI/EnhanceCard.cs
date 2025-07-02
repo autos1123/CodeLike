8934ed0d501e32af224be39224bd52b2701bfcb5
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-07-02 10:32:40 +0900

    add : 애니메이선 에셋

--- Assets/02.Scripts/UI/EnhanceCard.cs
+++ Assets/02.Scripts/UI/EnhanceCard.cs
@@ -38,7 +38,7 @@ public class EnhanceCard : MonoBehaviour
     }
     void Click()
     {
-        if(PlayerManager.Instance.Player.TryGetComponent<PlayerController>(out var playerController))
+        if(GameManager.Instance.Player.TryGetComponent<PlayerController>(out var playerController))
         {
             playerController.PlayerCondition.ChangeModifierValue(_enhanceData.ConditionType, ModifierType.BuffEnhance, _enhanceData.value);
             //todo : 강화 수치에 대핸 데이터를 넘겨준다.
