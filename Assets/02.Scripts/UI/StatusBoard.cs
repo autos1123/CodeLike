9af152f12077f92706c5ad03a90a003b67caef30
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-07-03 16:00:24 +0900

    Test : ui테스트 코드

--- Assets/02.Scripts/UI/StatusBoard.cs
+++ Assets/02.Scripts/UI/StatusBoard.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatusBoard : MonoBehaviour
+public class StatusBoard : UIBase
 {
+    public override string UIName => "StatusBoard";
+
     // Start is called before the first frame update
     void Start()
     {
