2d579e9cc6e8a985fb3c9246f408fbac34319b56
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-07-08 11:30:13 +0900

    feat : 플레이어 액티브 아이템 사용

--- Assets/02.Scripts/Unit/Player/PlayerActiveItemController.cs
+++ Assets/02.Scripts/Unit/Player/PlayerActiveItemController.cs
@@ -1,17 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+
+public enum Skillinput
+{
+    X = 0,
+    C = 1,
+}
 public class PlayerActiveItemController : MonoBehaviour
 {
     private Dictionary<SkillType, ISkillExecutor> executors;
+    private PlayerController playerController;
+    public ActiveItemData[] activeItemDatas;
 
-    private void Start()
+    private void Awake()
     {
+        playerController = GetComponent<PlayerController>();
+    }
+    private void Start()
+    {       
+
         executors = new Dictionary<SkillType, ISkillExecutor>
         {
             { SkillType.Projectile, new ProjectileSkillExecutor() },
-
         };
+        activeItemDatas = new ActiveItemData[2];
+    }
+
+    public void TakeItem(Skillinput skillinput, ActiveItemData activeItemData)
+    {
+        activeItemDatas[(int)skillinput] = activeItemData;
+    }
+
+    public void UseItem(Skillinput skillinput)
+    {
+        var used = TableManager.Instance.GetTable<ActiveItemEffectDataTable>().GetDataByID(activeItemDatas[(int)skillinput].skillID);
+        executors[used.Type].Execute(used, playerController.VisualTransform , playerController.VisualTransform.forward);
     }
 }
