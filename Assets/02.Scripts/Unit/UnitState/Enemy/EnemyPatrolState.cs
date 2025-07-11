2893e294de6a521ee70aac0e2b27ec34191f901f
lymsg <114712935+lymsg@users.noreply.github.com>
2025-07-11 15:23:45 +0900

    wip: 에너미 오류 수정중

--- Assets/02.Scripts/Unit/UnitState/Enemy/EnemyPatrolState.cs
+++ Assets/02.Scripts/Unit/UnitState/Enemy/EnemyPatrolState.cs
@@ -14,6 +14,7 @@ public class EnemyPatrolState : EnemyMoveState
     public override void StateEnter()
     {
         moveSpeedModifier = 1f;
+        stateMachine.Enemy.NavMeshAgent.isStopped = false; // NavMeshAgent를 정지시킴
         base.StateEnter();
 
         targetPos = stateMachine.PatrolPoint;
