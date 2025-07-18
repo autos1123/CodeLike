eb6e4be141da29a0ccd316a53b300c98c5f5168a
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-07-18 15:21:02 +0900

    feat : 다이얼로그 구현

--- Assets/02.Scripts/UI/DialogueBoard.cs
+++ Assets/02.Scripts/UI/DialogueBoard.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,14 +9,26 @@ public class DialogueBoard : UIBase
     [SerializeField] TextMeshProUGUI Conversation;
     [SerializeField] RawImage playerRawImage;
     [SerializeField] RawImage npcRawImage;
+    [SerializeField] Button beforButton;
+    [SerializeField] Button nextButton;
 
+    List<string> messages;
+    public int messagesPointer = 0;
     public override string UIName => this.GetType().Name;
 
+    public void Init(List<string> strings)
+    {
+        messages = strings;
+        messagesPointer = 0;
+    }
     public override void Open()
     {
         base.Open();
         playerRawImage.texture = DialogueManager.Instance.playerTexture;
         npcRawImage.texture = DialogueManager.Instance.npcTexture;
+        beforButton.onClick.AddListener(onClickBeforButton);
+        nextButton.onClick.AddListener(onClickNextButton);
+        show();
     }
     public override void Close()
     {
@@ -23,5 +37,25 @@ public class DialogueBoard : UIBase
         playerRawImage.texture = null;
         npcRawImage.texture = null;
     }
+    void onClickBeforButton()
+    {
+        messagesPointer = Math.Min(0, messagesPointer--);
+        show();
+    }
+    void onClickNextButton()
+    {
+        messagesPointer++;
+        if(messages.Count <= messagesPointer)
+        {
+            UIManager.Instance.Hide<DialogueBoard>();
+            UIManager.Instance.Hide<ShopUI>();
+            return;
+        }
+        show();
+    }
 
+    void show()
+    {
+        Conversation.text = messages[messagesPointer];
+    }
 }
