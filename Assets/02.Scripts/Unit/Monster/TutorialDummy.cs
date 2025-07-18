7f6242c2eeb3a78182a2fa38bff1b44904e0b2fe
hyeon11ok <hyeonsusin05@gmail.com>
2025-07-18 12:15:36 +0900

    feat : 튜토리얼 전용 몬스터 사망 및 드랍 기능 추가 구현

--- Assets/02.Scripts/Unit/Monster/TutorialDummy.cs
+++ Assets/02.Scripts/Unit/Monster/TutorialDummy.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class TutorialDummy : MonoBehaviour, IDamagable
 {
+    [SerializeField] private float hp = 100;
+    [SerializeField] private float dropGold = 10;
+    [SerializeField] private GameObject dropItemBox;
+
     private Animator anim;
+    private PlayerController player;
 
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        player = FindObjectOfType<PlayerController>();
     }
 
     public bool GetDamaged(float damage)
     {
+        if(hp <= 0) return false;
+
+        hp -= damage;
+        if(hp <= 0)
+        {
+            player.Condition.ChangeGold(dropGold);
+            anim.SetTrigger("Die");
+            Invoke(nameof(Die), 1f);
+
+            return false;
+        }
+
         anim.SetTrigger("Hit");
         return true;
+    }
+
+    private void Die()
+    {
+        Instantiate(dropItemBox, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
     }
 }
