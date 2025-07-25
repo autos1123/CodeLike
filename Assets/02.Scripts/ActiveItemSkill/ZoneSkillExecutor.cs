bf0958999c719ef412c979c5f49a09e55c882bfe
autos1123 <hea514@naver.com>
2025-07-25 19:54:32 +0900

    feat : 이펙트 구조변경 1차

--- Assets/02.Scripts/ActiveItemSkill/ZoneSkillExecutor.cs
+++ Assets/02.Scripts/ActiveItemSkill/ZoneSkillExecutor.cs
@@ -1,24 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ZoneSkillExecutor :ISkillExecutor
+public class ZoneSkillExecutor:ISkillExecutor
 {
     public void Execute(ActiveItemEffectData data, Transform caster, Vector3 targetPoint)
     {
         Debug.Log("targetPoint: " + targetPoint);
-        var vfx = Object.Instantiate(
-            Resources.Load<ParticleSystem>(data.VFX),
-            targetPoint,
-            Quaternion.identity
-        );
-        vfx.Play();
 
-        var obj = PoolManager.Instance.GetObject(PoolType.AoE);
-        obj.GetComponent<AoE>().Init(data);
-        obj.transform.position = targetPoint;
+        // 1. VFX를 PoolManager에서 꺼내서 사용
+        var vfxObj = PoolManager.Instance.GetObject(PoolType.VFX);
+        if(vfxObj != null)
+        {
+            vfxObj.transform.position = targetPoint;
+            vfxObj.transform.rotation = Quaternion.identity;
 
-        PoolManager.Instance.ReturnObject(obj.GetComponent<IPoolObject>(), 5);
+            var ps = vfxObj.GetComponent<ParticleSystem>();
+            if(ps != null)
+            {
+                ps.Play();
+                // 자동 반환: duration 후에 풀로 복귀
+                vfxObj.GetComponent<PooledVFX>()?.PlayVFX(ps.main.duration);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("VFX 풀에 오브젝트가 없습니다.");
+        }
+
+        // 2. Zone 오브젝트(AoE)도 풀에서 꺼내서 사용
+        //var obj = PoolManager.Instance.GetObject(PoolType.AoE);
+        //if(obj != null)
+        //{
+        //    obj.GetComponent<AoE>().Init(data);
+        //    obj.transform.position = targetPoint;
+        //    PoolManager.Instance.ReturnObject(obj.GetComponent<IPoolObject>(), 5);
+        //}
+        //else
+        //{
+        //    Debug.LogWarning("AoE 풀에 오브젝트가 없습니다.");
+        //}
     }
-
 }
