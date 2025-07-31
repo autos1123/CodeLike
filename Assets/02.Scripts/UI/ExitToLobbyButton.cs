1cd94e4ff54a7cf643e2470ecb0d7f1a3a46c299
autos1123 <hea514@naver.com>
2025-07-31 20:01:07 +0900

    feat : 엔딩씬 및 버튼추가

--- Assets/02.Scripts/UI/ExitToLobbyButton.cs
+++ Assets/02.Scripts/UI/ExitToLobbyButton.cs
@@ -5,19 +5,7 @@ public class ExitToLobbyButton:MonoBehaviour
 {
     public void OnClickExitToLobbyButton()
     {
+        SceneManager.LoadScene("TestIntroScene");
 
-        // 팝업으로 사용자에게 물어보고 이동하는 경우
-        ShowConfirmPopup(
-            "모든 정보를 잃고 로비씬으로 돌아갑니다",
-            () => { SceneManager.LoadScene("LobbyScene"); }, 
-            () => { }  
-        );
-    }
-
-    void ShowConfirmPopup(string message, System.Action onConfirm, System.Action onCancel)
-    {
-        bool userConfirmed = true; 
-        if(userConfirmed) onConfirm();
-        else onCancel();
     }
 }
