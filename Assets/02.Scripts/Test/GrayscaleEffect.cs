d819244b36dd55f927490e16708e182d7671b2c5
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-08-01 19:51:41 +0900

    fix : 책 RG없어서 버그 수정

--- Assets/02.Scripts/Test/GrayscaleEffect.cs
+++ Assets/02.Scripts/Test/GrayscaleEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class GrayscaleEffect : MonoBehaviour
 {
     public Material effectMaterial;
@@ -13,5 +13,30 @@ public class GrayscaleEffect : MonoBehaviour
             Graphics.Blit(source, destination, effectMaterial);
         else
             Graphics.Blit(source, destination); // 머티리얼 없으면 그대로 출력
+    }
+
+    private void OnEnable()
+    {
+        ViewManager.Instance.OnViewChanged += SwitchView;
+    }
+
+    private void OnDisable()
+    {
+        ViewManager.Instance.OnViewChanged -= SwitchView;
+    }
+
+    public void SwitchView(ViewModeType modeType)
+    {
+        if(modeType == ViewModeType.View3D)
+        {
+            effectMaterial.SetFloat("_GrayscaleAmount", 0);
+            effectMaterial.SetFloat("_Darken", -1f);
+        }
+        else
+        {
+            effectMaterial.SetFloat("_GrayscaleAmount", 1);
+            effectMaterial.SetFloat("_Darken", -0.5f);
+        }
+        
     }
 }
