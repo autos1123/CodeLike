e112140e24d28b28a0b60b41dfeeaa12ece557d0
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2025-08-01 14:48:47 +0900

    Wip : 사운드 위치 이동

--- Assets/02.Scripts/Sound/SoundSource.cs
+++ Assets/02.Scripts/Sound/SoundSource.cs
@@ -67,6 +67,6 @@ public class SoundSource : MonoBehaviour ,IPoolObject
     private void HandleViewModeChange(bool issfx)
     {
         audioSource.spatialBlend = 
-            (issfx || ViewManager.Instance.CurrentViewMode == ViewModeType.View3D) ? 1f : 0f;
+            (!issfx || ViewManager.Instance.CurrentViewMode == ViewModeType.View3D) ? 1f : 0f;
     }
 }
