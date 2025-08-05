236f456c01c18724397d8349c6fafa565572c4e2
hyeon11ok <hyeonsusin05@gmail.com>
2025-08-06 05:53:27 +0900

    feat : 룸생성시 서로 연결돼있는 룸은 같은 프리팹안쓰게끔

--- Assets/05.ScriptableObj/RoomPrefabSet.cs
+++ Assets/05.ScriptableObj/RoomPrefabSet.cs
@@ -13,17 +13,38 @@ public class RoomPrefabSet : ScriptableObject
     public GameObject shopRoom;
     public GameObject bossRoom;
 
+    private int n_prevRoomIdx = -1; // 직전에 생성된 일방 룸의 인덱스
+
     public GameObject GetRandomPrefab(RoomType type)
     {
         return type switch
         {
             RoomType.Start => startRoom,
             RoomType.End => endRoom,
-            RoomType.Normal => normalRooms[Random.Range(0, normalRooms.Count)],
+            RoomType.Normal => GetRandomNormalRoom(),
             RoomType.Shop => shopRoom,
             RoomType.Boss => bossRoom,
             _ => normalRooms[0],
             
         };
+    }
+
+    private GameObject GetRandomNormalRoom()
+    {
+        int idx;
+
+        if(n_prevRoomIdx == -1)
+        {
+            idx = Random.Range(0, normalRooms.Count);
+            n_prevRoomIdx = idx;
+            return normalRooms[idx];
+        }
+
+        do
+        {
+            idx = Random.Range(0, normalRooms.Count);
+        } while(idx == n_prevRoomIdx); // 직전 룸과 같은 인덱스는 제외
+        n_prevRoomIdx = idx;
+        return normalRooms[idx];
     }
 }
